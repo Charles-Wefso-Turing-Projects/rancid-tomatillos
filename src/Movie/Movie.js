44d4e9bfd220c1abb1e7948aa1dcdc6831832eef
Charles Wefso <cwefso@gmail.com>
2020-06-30 14:43:11 -0600

    :crystal_ball: adds rest of details to each Movie component

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -4,6 +4,10 @@ const Movie = ({movie}) => {
   return(
     <section className= "movie">
       <h3>{movie.title}</h3>
+      <h3>{movie.average_rating}</h3>
+      <h3>{movie.release_date}</h3>
+      <img src={movie.backdrop_path} alt="backdrop" className="backdrop"/>
+      <img src={movie.poster_path} alt="poster" className="poster"/>
     </section>
   )
 }
