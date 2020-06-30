ffee378709e08c36c68a2dcf197a68763317de31
Charles Wefso <cwefso@gmail.com>
2020-06-30 13:14:31 -0600

    set up App as a class component

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends {Component} {
+  constructor(){
+    super();
+    this.state = {
+      movies : []
+    }
+  }
+
+  render(){
+    return(
+      <main className= "App">
+
+      </main>
+    )
+  }
+}
+
+
+export default App;
