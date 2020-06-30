b411e21b0002894f1e7d4ddadd7780671ab1511d
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-06-30 15:07:08 -0600

    :tada: initial commit

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Movie from '/Users/cwefso/turing/module3/projects/rancid-tomatillos/src/Movie/Movie.js';
+import Movie from '../Movie/Movie.js';
+import './MoviesContainer.css'
 
 const MoviesContainer = ({movies}) => {
   const movieCards = movies.map(movie => (
