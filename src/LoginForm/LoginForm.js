b5c657076a5519d83f3b10b79cd9e9571ce3562c
Charles Wefso <cwefso@gmail.com>
2020-07-01 13:29:55 -0600

    :crystal_ball: Adds change handling to the login form

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -4,15 +4,34 @@ class LoginForm extends Component {
   constructor(){
     super();
     this.state = {
-      
+      username : "",
+      password : ""
     }
   }
 
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState({[name] : value})
+  } 
+
   render(){
+    const {username, password} = this.state
     return(
       <form>
-        <input />
-        <input />
+        <input 
+          type= "text"
+          name= "username"
+          value= {username}
+          placeholder= "username"
+          onChange= {this.handleChange}
+        />
+        <input 
+          type= "text"
+          name= "password"
+          value= {password}
+          placeholder= "password"
+          onChange= {this.handleChange}
+        />
         <button>Submit</button>
       </form>
     )
