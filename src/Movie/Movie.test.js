82e761d2f186bf87867d3c845ec901674a1608ba
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-04 09:29:15 -0600

    :bar_chart: Completed two Movie tests

--- src/Movie/Movie.test.js
+++ src/Movie/Movie.test.js
@@ -2,10 +2,34 @@ import React from "react";
 import Movie from "./Movie";
 
 import "@testing-library/jest-dom";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, getByAltText, getByText, getByLabelText } from "@testing-library/react";
 
 describe("Movie", () => {
   it("should render a title, img and average rating", () => {
     expect(true).toEqual(true);
   });
+
+  it("should display movies on load", () => {
+    const movie = {
+      id : "475430",
+      title  : "I'm a movie",
+      img : "poster",
+      rating : "5"
+    }
+
+    const { getByText, getByAltText, getByLabelText } = render(
+      <Movie 
+        movie = { movie }
+      />
+    );
+    
+    const rating = getByLabelText("average-rating")
+    const title = getByText("I'm a movie");
+    const img = getByAltText("poster");
+
+    expect(title).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(rating).toBeInTheDocument(); 
+  })
+
 });
