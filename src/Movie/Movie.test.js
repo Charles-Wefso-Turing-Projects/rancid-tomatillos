067ea220c00b9fe1456e74922b5a82a0ca3e371f
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-03 20:19:02 -0600

    :completed my first test:

---
+++ src/Movie/Movie.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Movie from "./Movie";
+
+import "@testing-library/jest-dom";
+import { render, fireEvent } from "@testing-library/react";
+
+describe("Movie", () => {
+  it("should render a title, img and average rating", () => {
+    expect(true).toEqual(true);
+  });
+});
