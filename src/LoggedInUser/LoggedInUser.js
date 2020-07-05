dae8a7f2468f700014fe0422732360ce0ce431a1
Charles Wefso <cwefso@gmail.com>
2020-07-05 17:12:31 -0600

    Adds functionality so that when a user is logged in and clicks on a movie, it renders a MovieDetailsPage component for that individual movie.

--- src/LoggedInUser/LoggedInUser.js
+++ src/LoggedInUser/LoggedInUser.js
@@ -1,18 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './LoggedInUser.css'
-import MoviesContainer from "../MoviesContainer/MoviesContainer.js";
+import MoviesContainer from "../MoviesContainer/MoviesContainer";
+import MovieDetailsPage from "../MovieDetailsPage/MovieDetailsPage"
 
 
-const LoggedInUser = ({movies, loggedInUserData, refreshPage, showMovieDetailsPage}) => {
-  const name = loggedInUserData.data.user.name
-  return(
-    <main className="LoggedInUserMainPage">
-      <h1>Hello { name }</h1>
-      <button aria-label="logoutButton" onClick={refreshPage}>Logout</button>
-      
-      <MoviesContainer movies={movies} showMovieDetailsPage= {showMovieDetailsPage}/>
-    </main>
-  )
+class LoggedInUser extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedMovie : null
+    }
+    this.url = "https://rancid-tomatillos.herokuapp.com/api/v2"
+  }
+
+  setID = (e) => {
+    const {id} = e.target.closest(".movie");
+    this.getMovieData(id)
+  }
+
+  resetMovie = () => {
+    this.setState({
+      selectedMovie : null
+    })
+  }
+
+  getMovieData(id) {
+    fetch(`${this.url}/movies/${id}`)
+    .then((res) => res.json())
+    .then(
+      (data) => {
+        this.setState({
+          selectedMovie : data.movie
+        })
+      })
+    .catch((error) => {
+      console.log(error)
+      alert(`yo, this is wrong:  ${error}`)
+    })
+  }
+
+  render(){
+    if(this.state.selectedMovie !== null) {
+      return(
+        <section>
+          <MovieDetailsPage movie= {this.state.selectedMovie} resetMovie= {this.resetMovie}/>
+        </section>
+      )
+    } else {
+    return(
+      <main className="LoggedInUserMainPage">
+        <h1>Hello { this.props.name }</h1>
+        <button aria-label="logoutButton" onClick={this.props.refreshPage}>Logout</button>
+        <MoviesContainer movies={this.props.movies} setID= {this.setID} getMovieData= {this.getMovieData}/>
+      </main>
+    )}
+  }
 }
 
 export default LoggedInUser
