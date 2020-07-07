bb257de2808b381d9479b3db629b7e604baf22df
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-07 16:42:17 -0600

    :bar_chart: Should run resetMovie on the click of home button

--- src/MovieDetailsPage/MovieDetailsPage.test.js
+++ src/MovieDetailsPage/MovieDetailsPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import MovieDetailsPage from "./MovieDetailsPage"
 
 import "@testing-library/jest-dom";
-import { render, fireEvent, getByAltText, getByText, getByLabelText } from "@testing-library/react";
+import { render, getByDisplayValue } from "@testing-library/react";
 
 describe("MoveDetailsPage", () => {
   it("should render a movies details", () => {
@@ -11,27 +11,56 @@ describe("MoveDetailsPage", () => {
 
   it("Should run resetMovie on the click of home button", () => {
     // SetUp
-    const { getByText, getByRole } = render(<MovieDetailsPage 
-      // movie = {
-      //   <section style={{backgroundImage: 'url(' + movie.backdrop_path + ')'}} className= "movie-details-page">
-      //     <h2>{movie.title}</h2>
-      //     <h3>{movie.tagline}</h3>
-      //     <p>Overview: {movie.overview}</p>
-      //     <p>Release Date: {movie.release_date}</p>
-      //     <p>Average Rating: {movie.average_rating}</p>
-      //     <p>Genres: {movie.genres.join(", ")}</p>
-      //     <p>Runtime: {movie.runtime} minutes</p>
-      //     <button onClick= {resetMovie}>Home</button>
-      //   </section>  
-      // }
-  
-    />)
-  
+    const { getByText, getByRole } = render
+    (<MovieDetailsPage 
+          id={1}
+          title="movie title"
+          tagline="catchy movie tagline"
+          overview="movie overview, but with spoilers"
+          releaseDate="2020/01/01"
+          averageRating={3}
+          genres="romance, action, drama"
+          runtime={300}
+          resetMovie={jest.fn()}
+      />
+    )
       // Execution
+      const title = getByText("movie title");
+      const tagline = getByText("catchy movie tagline");
+      const overview = getByText("movie overview, but with spoilers");
+      const releaseDate = getByText("2020/01/01")
+      const averageRating = getByDisplayValue(3)
+      const genres = getByText("romance, action, drama");
+      const runtime = getByDisplayValue(300);
+      const button = getByRole("button");
   
       // Assertion
+      expect(title).toBeInTheDocument()
+      expect(tagline).toBeInTheDocument()
+      expect(overview).toBeInTheDocument()
+      expect(releaseDate).toBeInTheDocument()
+      expect(averageRating).toBeInTheDocument()
+      expect(genres).toBeInTheDocument()
+      expect(runtime).toBeInTheDocument()
+      expect(button).toBeInTheDocument()
+  })
 
-
+  it("should run resetMovie", () => {
+    // Setup
+    const mockResetMovie = jest.fn();
+    const { getByText, getByRole } = render
+    (<MovieDetailsPage 
+          id={1}
+          title="movie title"
+          tagline="catchy movie tagline"
+          overview="movie overview, but with spoilers"
+          releaseDate="2020/01/01"
+          averageRating={3}
+          genres="romance, action, drama"
+          runtime={300}
+          resetMovie={mockResetMovie}
+      />
+    )
 
   })
 
