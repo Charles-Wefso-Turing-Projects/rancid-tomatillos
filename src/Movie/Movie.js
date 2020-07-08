225fd1654e755e69affb7a18365660e30398bd9b
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-07-08 15:01:16 -0600

    :crystal_ball: added average ratings

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -9,12 +9,13 @@ const Movie = ({movie, setID, getMovieData, id}) => {
            alt="poster" 
            className="poster"
       />
-      <h3 aria-label="average-rating"></h3>
+      <h3 aria-label="average-rating">Average Rating: { movie.average_rating.toFixed(2)}</h3>
       {/* <h3>{movie.release_date}</h3> */}
       {/* <img src={movie.backdrop_path} alt="backdrop" className="backdrop"/> */}
       
     </section>
   )
 }
+
 
 export default Movie
