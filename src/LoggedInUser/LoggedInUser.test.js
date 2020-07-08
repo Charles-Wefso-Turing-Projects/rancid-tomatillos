181ec88044b25fa9cd1cc911d2a21567aa59f42e
Charles Wefso <cwefso@gmail.com>
2020-07-07 19:22:16 -0600

    updates testing library

---
+++ src/LoggedInUser/LoggedInUser.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import LoggedInUser from './LoggedInUser';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+
+describe('LoggedInUser', () => {
+
+  it('Should render a MovieContainer, a button, and an h1', () => {
+    //Setup
+    const { getByLabelText, getByRole, getByText } = render(<LoggedInUser 
+                movies= {[]} 
+                loggedInUserData={ {} } 
+    />)
+  
+    //Execution
+    const MovieContainer = getByLabelText("all-movies");
+    const button = getByRole("button")
+    const header = getByText("Hello")
+    //Assertion
+
+    expect(MovieContainer).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+    expect(header).toBeInTheDocument()
+  });
+
+
+ //It should return a successful response if POST is made with correct inputs
+
+ //It should return an error if the information was incorrect
+
+ 
+
+});
