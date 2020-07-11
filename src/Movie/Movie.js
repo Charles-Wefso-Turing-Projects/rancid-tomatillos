6615e9f6770603643b23ee8b65cae61db65595c6
Charles Wefso <cwefso@gmail.com>
2020-07-11 09:48:00 -0600

    :crystal_ball: displays userRatings on rated movies

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,33 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
 import './Movie.css'
 
-const Movie = ({movie, setID, userRatings, id, loggedIn}) => {
-// if user is logged in, render this with user button, else return below
-  if(loggedIn) {
+class Movie extends Component {
+  constructor({movie, setID, userRatings, id, loggedIn}){
+    super({movie, setID, userRatings, id, loggedIn});
+    this.state = {
+      rated: false,
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    if (this.props.userRatings !== prevProps.userRatings) {
+        this.setState({
+          rated: true
+        })
+    }
+  }
+
+  
+  render() {
+
+
+    if(this.state.rated === true && this.props.userRatings !== null) {
+      const movieUserRating = this.props.userRatings.ratings.map(rating => {
+        if(rating.movie_id === this.props.movie.id){
+          return rating.rating
+        }
+    })  
+
+      return(
+          <section className= "movie" id={this.props.id}>
+            <h2 >{this.props.movie.title} </h2>
+            <img src={this.props.movie.poster_path} 
+              alt="poster" 
+              className="poster"
+              />
+            <h3 aria-label="average-rating">Average Rating: { this.props.movie.average_rating.toFixed(2)}</h3>
+            <h3 aria-label="user-rating">Your Rating: { movieUserRating }</h3>
+            <button>Logged In</button>
+          </section>
+      )
+    }
+
     return(
-        <section className= "movie" id={id}>
-          <h2 >{movie.title} </h2>
-          <img src={movie.poster_path} 
-             alt="poster" 
-             className="poster"
-             />
-          <h3 aria-label="average-rating">Average Rating: { movie.average_rating.toFixed(2)}</h3>
-          <button>Logged In</button>
-        </section>
+      <section className= "movie" id={this.props.id} onClick= {this.props.setID} >
+      <h2 >{this.props.movie.title} </h2>
+      <img src={this.props.movie.poster_path} 
+            alt="poster" 
+            className="poster"
+            />
+      <h3 aria-label="average-rating">Average Rating: { this.props.movie.average_rating.toFixed(2)}</h3>
+    </section>
     )
   }
-  return(
-    <section className= "movie" id={id} onClick= {setID} >
-      <h2 >{movie.title} </h2>
-      <img src={movie.poster_path} 
-           alt="poster" 
-           className="poster"
-      />
-      <h3 aria-label="average-rating">Average Rating: { movie.average_rating.toFixed(2)}</h3>
-    </section>
-  )
 }
 
-
 export default Movie
+
+
+// if user is logged in, render this with user button, else return below
+
+
+
