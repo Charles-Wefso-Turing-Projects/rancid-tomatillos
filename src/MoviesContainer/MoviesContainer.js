dfd585be5b082261304d06e63b49391698c717db
Charles Wefso <cwefso@gmail.com>
2020-07-12 08:39:49 -0600

    :crystal_ball: Converts MoviesContainer component back into functional component

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,51 +1,39 @@
-import React, { Component } from "react";
+import React, from "react";
 import Movie from "../Movie/Movie.js";
 import "./MoviesContainer.css";
 import { Link } from "react-router-dom";
 
-class MoviesContainer extends Component {
-  constructor({
-    movies,
-    setID,
-    getMovieData,
-    loggedIn,
-    userRatings,
-  }) {
-    super({
-      movies,
-      setID,
-      getMovieData,
-      loggedIn,
-      userRatings,
-    });
-  }
+const MoviesContainer = ({
+  movies,
+  setID,
+  getMovieData,
+  loggedIn,
+}) => {
 
-  render() {
-    const movieCards = this.props.movies.map((movie) => (
-      <Link
-        to={`/${movie.id}`}
-        key={movie.id}
-        style={{ textDecoration: "none", color: "black" }}
-      >
+  const movieCards = movies.map((movie) => (
+    <Link
+    to={`/${movie.id}`}
+    key={movie.id}
+    style={{ textDecoration: "none", color: "black" }}
+    >
         <Movie
           movie={movie}
           id={movie.id}
           key={movie.id}
-          setID={this.props.setID}
-          getMovieData={this.props.getMovieData}
-          selectedMovie={this.props.selectedMovie}
-          loggedIn={this.props.loggedIn}
+          setID={setID}
+          getMovieData={getMovieData}
+          selectedMovie={selectedMovie}
+          loggedIn={loggedIn}
           rated={movie.rated}
-        />
+          />
       </Link>
     ));
-
+    
     return (
       <section aria-label="all movies" className="allMovies">
         {movieCards}
       </section>
     );
   }
-}
 
 export default MoviesContainer;
