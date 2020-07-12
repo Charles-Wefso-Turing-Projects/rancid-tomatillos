40a1f6bfee768bf58f907637a308a17d04586d58
Charles Wefso <cwefso@gmail.com>
2020-07-12 12:02:49 -0600

    :bar_chart: creates unit tests for Movie component

--- src/Movie/Movie.test.js
+++ src/Movie/Movie.test.js
@@ -1,41 +1,89 @@
 import React from "react";
 import Movie from "./Movie";
-
 import "@testing-library/jest-dom";
-import { render, fireEvent, getByAltText, getByText, getByLabelText } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 
-//If not logged in should show title, poster, average rating
-
-//If logged in and movie is rating should see user rating
-
-//If logged in and movie is not rated should see Not Yet Rated
 
 describe("Movie", () => {
-  it("should render a title, img and average rating", () => {
-    expect(true).toEqual(true);
-  });
 
   it("should display movies on load", () => {
     const movie = {
       id : "475430",
       title  : "I'm a movie",
       img : "poster",
-      rating : "5"
+      rating : "5",
+      average_rating : 8
     }
 
     const { getByText, getByAltText, getByLabelText } = render(
-      <Movie 
-        movie = { movie }
-      />
-    );
-    
-    const rating = getByLabelText("average-rating")
+      <BrowserRouter><Movie movie = {movie}/></BrowserRouter>
+    )
+      
+    const averageRating = getByLabelText("average-rating")
     const title = getByText("I'm a movie");
     const img = getByAltText("poster");
 
     expect(title).toBeInTheDocument();
     expect(img).toBeInTheDocument();
-    expect(rating).toBeInTheDocument(); 
+    expect(averageRating).toBeInTheDocument(); 
+  })
+
+  it("should display user rating if user is logged in", () => {
+    const movie = {
+      id : "475430",
+      title  : "I'm a movie",
+      img : "poster",
+      rated : "5",
+      average_rating : 8,
+    }
+
+    const { getByLabelText } = render(
+      <MemoryRouter><Movie movie = {movie} loggedIn= {true}/></MemoryRouter>
+    )
+
+    const userRating = getByLabelText("user-rating")
+
+    expect(userRating).toBeInTheDocument();
+
+  })
+
+  it("should display 'No User Rating' if the movie has not been rated", () => {
+    const movie = {
+      id : "475430",
+      title  : "I'm a movie",
+      img : "poster",
+      rated : null,
+      average_rating : 8,
+    }
+
+    const { getByText } = render(
+      <MemoryRouter><Movie movie = {movie} loggedIn= {true}/></MemoryRouter>
+    )
+    
+    const noRating = getByText("No User Rating")
+
+    expect(noRating).toBeInTheDocument()
+  })
+
+  it("should display user rating if the movie has been rated", () => {
+    const movie = {
+      id : "475430",
+      title  : "I'm a movie",
+      img : "poster",
+      rated : 5,
+      average_rating : 8,
+    }
+    
+    console.log(movie)
+
+    const { getByText } = render(
+      <MemoryRouter><Movie movie = {movie} loggedIn= {true} rated= {movie.rated}/></MemoryRouter>
+    )
+    
+    const rating = getByText("Your rating : 5")
+
+    expect(rating).toBeInTheDocument()
   })
 
 });
